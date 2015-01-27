e600f59beaf212c28cab0e13769320c82c8bd3d7
Michael Sayer <michael.j.say@gmail.com>
2015-01-26 20:54:58 -0700

    done except for step 6

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,34 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData) {
+  $scope.teamData = teamData;
+  $scope.newGame = {};
+  $scope.showNewGameForm = false;
+  $scope.toggleNewGameForm = function() {
+  	showNewGameForm = !showNewGameForm;
+  }
+  if ($routeParams.team === 'utahjazz') {
+  	$scope.homeTeam = 'Utah Jazz';
+  	$scope.logoPath = 'images/jazz-logo.png';
+  } 
+  if ($routeParams.team === 'losangeleslakers') {
+  	$scope.homeTeam = 'Los Angeles Lakers';
+  	$scope.logoPath = 'images/lakers-logo.png';
+  } 
+  if ($routeParams.team === 'miamiheat') {
+  	$scope.homeTeam = 'Miami Heat';
+  	$scope.logoPath = 'images/heat-logo.png';
+  }
 
+  $scope.submitGame = function() {
+  	$scope.newGame = $scope.homeTeam.split('').join('').toLowerCase();
+    teamService.addNewGame($scope.newGame).then(function() {
+    teamService.getTeamData($scope.newGame.homeTeam).then(function(results) {
+            $scope.teamData = results;
+            $scope.newGame = {};
+            $scope.showNewGameForm = false;
+    	})
+    })
+  }
+console.log($scope);
 });
